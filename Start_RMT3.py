ace46c512451ede78092e59f646ba0f107f8da00
Silvestro Poccia <silvestro.poccia@gmail.com>
2023-02-25 12:41:17 +0100

    setup linspace fpr  gfilter

--- Start_RMT3.py
+++ Start_RMT3.py
@@ -22,14 +22,22 @@ def compute_distance_matrix(locations):
             #parallelDist(point_i,distance_matrix,i,locations)
     distance_matrix = (distance_matrix - np.min(distance_matrix)) / np.ptp(distance_matrix)
     return distance_matrix
-def plot_gaussian(sigma):
+
+
+def prepare_gaussians(sigma, mustprint):
     mu = 0
     x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
     #print(sigma.shape)
+    smoothing_curve= np.zeros(sigma.shape[0])
+    smoothing_curve = stats.norm.pdf(x, mu, sigma[0])
     for i in range(sigma.shape[0]):
-        plt.plot(x, stats.norm.pdf(x, mu, sigma[i]))
-    plt.legend(sigma)
-    plt.show()
+        smoothing_curve[:,i] = stats.norm.pdf(x[:,i], mu, sigma[i])
+        if(mustprint != 0):
+            plt.plot(x, smoothing_curve[i])
+    if (mustprint != 0):
+        plt.legend(sigma)
+        plt.show()
+    return smoothing_curve
 # load  dependency file
 
 def normalizzarighe(H):
@@ -42,7 +50,7 @@ def computeDependencyScale( H, sigma ):
     W = math.ceil(4 * sigma)
     # crea un vettore di smoothing 1 riga  e 2*W colonne
     gaussFunc = np.zeros(2*W)
-    acc = 0;
+    acc = 0
     for j in range(2 * W):
         gaussFunc[j] = math.exp(-(j - W - 1) ** 2 / (2 * sigma ** 2))
         acc = acc + gaussFunc[j]
@@ -74,7 +82,8 @@ kdepd = 2**(stepD) # step of scale for smoothing dependency
 
 scale_sigmaT = cnf.sigmaT * ktime
 scale_sigmaD = cnf.sigmaD * kdepd #cnf.sigmaD/(cnf.scaledpd+1))
-plot_gaussian(scale_sigmaD[0:1])
+smoothing_curve_dep = prepare_gaussians(scale_sigmaD, 0) #[0:2], 0)
+smoothing_curve_time = prepare_gaussians(scale_sigmaT, 0)
 #print(scale_sigmaD)
 """per ogni scala faccio uno smoothing della matrice delle dipendenze e poi la si taglia  creando 
 la finestra di smoothing 
--- config/configMocap.py
+++ config/configMocap.py
@@ -25,7 +25,7 @@ scaletime = 5     #:  Number of maximum scale over Time in an octave teh consecu
 scaledpd = 5     #:  Number of maximum scale over Dependency
 n_octaveDPD = 0
 n_octaveTIME = 2
-sigmaT = 0
+sigmaT = 4
 sigmaD = 0.5 #DeSigmaDepd = 0.5;
 
 sigmaNT = 0.0 # sipresuppone che  il dto  abbia subito uno smoothing iniziale di una gaussiana di 0.5, lo metto a 0 per ora
